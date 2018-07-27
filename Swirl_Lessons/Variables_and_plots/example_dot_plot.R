de2cfcd2138b8e309e7a4a8ed8234983235716de
datanigu <datanigu@mit.edu>
2018-07-26 22:56:54 -0700

    Removing example_dot_plot.R

--- Swirl_Lessons/Variables_and_plots/example_dot_plot.R
+++
@@ -1,7 +0,0 @@
-# Example dot plot
-# - Class: text
-# Output: One type of plot is a DOT PLOT.  In a dot plot, there is a horizontal x-axis, and the data are visualized as dots above this x-axis. 
-par(mar=c(1,1,1,1))
-dotchart(c(13,14,15,23),labels=c("A","B","C","D"))
-
-         
