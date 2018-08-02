2099e0cdfee9c6074f8c3b705bd6a8e8df2e1a53
jhazlehurst <jhazlehu@gmail.com>
2018-08-01 18:51:37 -0700

    Changed questions

--- Swirl_Lessons/Binomial_Test/Figure2.R
+++ Swirl_Lessons/Binomial_Test/Figure2.R
@@ -1,11 +1,3 @@
 #Figure 2
 
-
-plot.binomial.distribution<- function(n,p){
-  x<- dbinom(0:n, size=n, prob=p)
-  return(
-    barplot(x,names.arg=0:n,ylim=c(0,1.0),xlab="Number of Desired Outcomes", ylab="Probability of Outcome", main="Binomial Distribution")
-  )
-}
-
 plot.binomial.distribution(100,0.5)
